d7bc559af5bc6dc13cf7ffcace65568e91217fda
hannah-scott <hannahscott@tutanota.com>
2020-11-16 09:43:20 +0000

    Added functionality to fill screen

--- tutorials/basics/code/barchart.js
+++ tutorials/basics/code/barchart.js
@@ -1,7 +1,16 @@
-var svg = d3.select("svg"),
-    margin = 100,
-    width = svg.attr("width") - margin ,
-    height = svg.attr("height") - margin;
+var width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var margin = 100;
+
+width -= margin;
+height = width / 1.6 + margin;
+
+svg = d3.select("body")
+    .append("svg")
+    .attr("width", width + margin)
+    .attr("height", height + margin);
 
 var xScale = d3.scaleBand().range ([0, width]).padding(0.4),
     yScale = d3.scaleLinear().range([height, 0]);
